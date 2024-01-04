435e02a1e8c040fc3dfa0a124491de8df08800d8
Pijussad <64306577+Pijussad@users.noreply.github.com>
2024-01-04 08:48:52 +0200

    Update LectureDApp.sol

--- my-contract/contracts/LectureDApp.sol
+++ my-contract/contracts/LectureDApp.sol
@@ -2,15 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract LectureDApp {
-    mapping(address => bool) public isTeacher;
-    mapping(address => uint) public userBalances; // New mapping to store user balances
+    mapping(address => bool) public isTeacher; // mappingas ar mokytojas ar ne
+    mapping(address => uint) public userBalances; //mappingas kad susieti su id numeriu
 
     uint public lectureCount = 0;
     mapping(uint => Lecture) public lectures;
 
+    //eventai
     event LectureUploaded(uint id, string link, string information, uint price);
     event LecturePurchased(uint id, address buyer);
 
+    //kad galetu tik mokytojas
     modifier onlyTeacher() {
         require(isTeacher[msg.sender], "Only teachers can call this function");
         _;
@@ -20,20 +22,20 @@ contract LectureDApp {
         isTeacher[msg.sender] = true;
     }
 
+    //mokytojo funkcijos
     function addTeacher(address _teacher) public onlyTeacher {
         isTeacher[_teacher] = true;
     }
-
     function uploadLecture(string memory _link, string memory _information, uint _price) public onlyTeacher {
     emit LectureUploaded(++lectureCount, _link, _information, _price);
 
-    // Set teacher to the deployer's address
+    
+    //bendros funkcijos
     lectures[lectureCount] = Lecture(lectureCount, _link, _information, _price, payable(msg.sender));
-    // Update the teacher field in the Lecture struct
     lectures[lectureCount].teacher = payable(msg.sender);
 }
 
-
+    
     function viewAvailableLectures() public view returns (Lecture[] memory) {
         Lecture[] memory allLectures = new Lecture[](lectureCount);
         for (uint i = 1; i <= lectureCount; i++) {
@@ -49,16 +51,14 @@ contract LectureDApp {
         require(lecture.price > 0, "Invalid lecture price");
         require(msg.value >= lecture.price, "Insufficient funds");
 
-        // Transfer funds to the teacher
         lecture.teacher.transfer(lecture.price);
 
-        // Add the ether to the teacher's balance
         userBalances[lecture.teacher] += lecture.price;
 
         emit LecturePurchased(_id, msg.sender);
     }
 
-    struct Lecture {
+    struct Lecture { //pamokos info
         uint id;
         string link;
         string information;
